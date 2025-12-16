98fa22410f9649a574e50d41d07220b9d74d97ea
Harpanada <liyovanh@gmail.com>
2025-12-16 20:52:32 +0700

    Latihan Dari Course

---
+++ quiz_nilai_rata-rata.py
@@ -0,0 +1,9 @@
+var_array = [i for i in range(101)]
+
+for i in range(len(var_array)):
+    jumlah=var_array[0]
+    for j in range(i+1):
+        jumlah += var_array[j]
+
+rata_rata=jumlah/len(var_array)
+print(f"Rata-rata dari 0 sampai 100 adalah: {rata_rata}")
