4c64e663f87bc785dd84bc2c3936ececb5195c7a
Harpanada <liyovanh@gmail.com>
2025-12-16 22:39:44 +0700

    Latihan Memahami Matriks

---
+++ matriks.py
@@ -0,0 +1,26 @@
+# baris= input("Masukan besar baris matriks: ")
+# kolom= input("Masukan besar kolom matriks: ")
+
+# #Matriks dengan default value= 0
+# matriks=[[0 for i in range(int(baris))] for j in range(int(kolom))]
+# print(matriks)
+
+
+nilai_mtk =     [80, 90, 75, 85, 60]
+nilai_bindo =   [70, 60, 65, 75, 80]
+nilai_inggris = [85, 80, 90, 70, 75]
+nilai_fisika =  [90, 85, 80, 95, 70]
+
+nilai_siswa = [  nilai_mtk ,nilai_bindo ,nilai_inggris,nilai_fisika]
+
+print(nilai_siswa[3][1])
+
+
+
+var_mat = [[1, 2, 3, 4, 5],
+           [6, 7, 8, 9, 10],
+           [11, 12, 13, 14, 15],
+           [16, 17, 18, 19, 20],
+           [21, 22, 23, 24, 25]]
+           
+print(var_mat[2][1])
